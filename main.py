829bd8340e6fe39a35ebd133696c8917b63c66b0
PiotrDuda2004 <piotr.duda2004@onet.pl>
2024-01-17 22:00:27 +0100

    reverted README.md file, Updated main.py. Closes #3

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from rich.console import Console
 
 if __name__ == '__main__':
     console = Console()
-    console.print("Hello", "World!", style="bold red")
+    console.print("Hello World!", style="bold red")
